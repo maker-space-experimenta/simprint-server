4616c04fc3221c87a9c363633585d28eaac2a242
harmoniemand <github@hmnd.de>
2022-12-03 22:21:45 +0100

    first test of the frontend for the slicer integration

--- webapp/src/app/slicer/components/upload/upload.component.ts
+++ webapp/src/app/slicer/components/upload/upload.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-upload',
@@ -12,18 +14,28 @@ export class UploadComponent implements OnInit {
   @Input() data: any;
   @Output() dataChange = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
 
   onFileSelected(e: any) {
-    const file:File = e.target.files[0];
+    const file: File = e.target.files[0];
 
     if (file) {
+      // this.fileName = file.name;
+      // this.data.file = file;
 
-        this.fileName = file.name;
-this.data.file = file;
+      let formData = new FormData();
+      formData.append("file", file);
+
+      let upload_url = "http://172.18.5.196:5000/api/slicer";
+      this.http.post(upload_url, formData).subscribe(result => {
+        console.log(result);
+      })
+
     }
   }
 
